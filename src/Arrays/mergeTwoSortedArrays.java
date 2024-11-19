7708c57170dd7b09d34d378c93d2bfac346056ac
Faique Inaas MZM <faiqueinaas@gmail.com>
2024-11-19 15:37:33 -0800

    added a question class

---
+++ src/Arrays/mergeTwoSortedArrays.java
@@ -0,0 +1,37 @@
+package Arrays;
+
+//merge two sorted integer arrays
+
+import java.util.Arrays;
+
+public class mergeTwoSortedArrays {
+
+    public static int[] mergeSortedArrays(int[] arr1, int[] arr2) {
+        int[] result = new int[arr1.length + arr2.length];
+        int i = 0, j = 0, k = 0;
+        while (i < arr1.length && j < arr2.length) {
+            if (arr1[i] < arr2[j]) {
+                result[k++] = arr1[i++];
+            } else {
+                result[k++] = arr2[j++];
+            }
+        }
+        while (i < arr1.length) {
+            result[k++] = arr1[i++];
+        }
+        while (j < arr2.length) {
+            result[k++] = arr2[j++];
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] arr1 = {1, 2, 3, 5, 8, 10};
+        int[] arr2 = {2, 4, 6, 7, 8, 11};
+        int[] result = mergeSortedArrays(arr1, arr2);
+
+        System.out.println("First array elements : " + Arrays.toString(arr1));
+        System.out.println("Second array elements : " + Arrays.toString(arr2));
+        System.out.println("Merged final array elements : " + Arrays.toString(result));
+    }
+}
